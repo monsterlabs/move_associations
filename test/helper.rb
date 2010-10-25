510bb6b2b994d6170e1e6cf2409939704dfef359
juarlex <alex@monsterlabs.com.mx>
2010-10-25 18:15:12 -0500

    Initial commit to move_associations.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'move_associations'
+
+class Test::Unit::TestCase
+end
